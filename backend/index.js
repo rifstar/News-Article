e91ae8ad2925ab09cf75cab933d19907818cc359
rifstar <mhmdarif0503@gmail.com>
2025-01-06 18:23:06 +0700

    add update user api route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -18,12 +21,14 @@ const app = express()
 
 // for allowing json object in req body
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000!")
 })
 
 app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
