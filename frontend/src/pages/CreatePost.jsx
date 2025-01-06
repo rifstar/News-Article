6d776bf6123e7ce138d5672237c6b09fd1f9a991
rifstar <mhmdarif0503@gmail.com>
2025-01-07 03:11:20 +0700

    upload image functionality in create article page

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -9,11 +9,51 @@ import {
   SelectTrigger, 
   SelectValue 
 } from "@/components/ui/select";
-import React from "react";
+import { useToast } from "@/hooks/use-toast";
+import { getFilePreview, uploadFile } from "@/lib/appwrite/uploadImage";
+import React, { useState } from "react";
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
 
 const CreatePost = () => {
+  const {toast} = useToast()
+  const [file, setFile] = useState(null)
+  const [imageUploadError, setImageUploadError] = useState(null)
+  const [imageUploading, setImageUploading] = useState(false)
+
+  const [formData, setFormData] = useState({})
+
+  const handleUploadImage = async () => {
+    try {
+      if(!file){
+        setImageUploadError("Please select an image!")
+        toast({title: "Please select an image!"})
+        return
+      }
+
+      setImageUploading(true)
+
+      setImageUploadError(null)
+
+      const uploadedFile = await uploadFile(file)
+      const postImageUrl = getFilePreview(uploadedFile.$id)
+
+      setFormData({...formData, image: postImageUrl})
+
+      toast({title: "Image Uploaded Successfully!"})
+
+      if(postImageUrl){
+        setImageUploading(false)
+      }
+    } catch (error) {
+      setImageUploadError("Image upload failed")
+      console.log(error)
+
+      toast({title: "Image upload failed!"})
+      setImageUploading(false)
+    }
+  }
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold text-slate-700">Create a post</h1>
@@ -42,15 +82,27 @@ const CreatePost = () => {
           <Input
             type="file"
             accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
           />
 
           <Button
             type="button"
             className="bg-slate-700"
+            onClick={handleUploadImage}
           >
-            Upload Image
+            {imageUploading ? "Uploading..." : "Upload Image"}
           </Button>
         </div>
+
+        {imageUploadError && <p className="text-red-600">{imageUploadError}</p>}
+
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt="upload"
+            className="w-full h-72 object-cover"
+          />
+        )}
 
         <ReactQuill
           theme="snow"
