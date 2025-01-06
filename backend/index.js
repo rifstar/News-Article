9f17c16286b3844c766c16c6d3fadd272a641083
rifstar <mhmdarif0503@gmail.com>
2025-01-07 02:38:41 +0700

    create post api route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
-import cookieParser from "cookie-parser"
+import postRoutes from "./routes/post.route.js"
 
 dotenv.config()
 
@@ -29,6 +30,7 @@ app.listen(3000, ()=>{
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
+app.use("/api/post", postRoutes)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
